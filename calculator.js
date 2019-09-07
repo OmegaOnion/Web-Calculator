a5e3dfd8a9fb924054370fd1d22c2027c53e3812
Sam Harris VM <omegaonion@gmail.com>
2019-09-07 21:00:39 +0100

    basic functionality added

--- calculator.js
+++ calculator.js
@@ -16,3 +16,73 @@ function operate(a,b,operator){
     operator == '*' ? multiply(a,b) :
     divide(a,b)
 }
+function addButtonListeners(){
+    // number buttons
+    var classname = document.getElementsByClassName("number");
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', numberClick);
+    }
+    // operator buttons
+    var classname = document.getElementsByClassName("operator");
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', operatorClick);
+    }
+    //function buttons
+    document.getElementById("equals").addEventListener('click', calculate);//equals
+    document.getElementById("clear").addEventListener('click', clear);//clear
+
+}
+function numberClick(){
+   var display = getDisplay();
+   if (currentNum="") currentNum = String(this.value);
+   else currentNum += String(this.value)
+   display.innerHTML+= this.value;
+}
+
+function operatorClick(){
+    var display = getDisplay();
+    a = parseInt(currentNum);
+    currentNum="";
+    currentOperator = this.value;
+    display.innerHTML+= " " + this.value + " ";
+ }
+
+function clear(){
+    getDisplay().innerHTML="";
+    getOutput().innerHTML = "";
+    resetValues(); 
+}
+function calculate(){
+    var display = getDisplay();
+    display.innerHTML+= " " +  "=";
+    b = parseInt(currentNum);
+    var answer = operate(a,b,currentOperator);
+    outputValue(answer);
+    resetValues();
+}
+
+function outputValue(text){
+   getOutput().innerHTML=text;
+}
+
+function getDisplay(){
+    return document.getElementById("inputDisplay");
+}
+function getOutput(){
+    return document.getElementById("outputDisplay")
+}
+function resetValues(){
+    currentNum = "";
+    a = 0;
+    b = 0;
+    currentOperator = "";
+}
+
+function onLoad(){
+    addButtonListeners();
+}
+
+var currentNum="";
+var a;
+var b;
+var currentOperator;
